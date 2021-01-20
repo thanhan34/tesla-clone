fe5f3aec5fd2f0b3d443b260a43af3fae1bfae27
An Doan <dtan42@gmail.com>
2021-01-20 23:18:27 +1000

    menu

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './App.css';
 import Header from './Header';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import Menu from './Menu';
 function App() {
 
   const [isMenuOpen, setIsMenuOpen] = useState(false)
@@ -11,6 +12,7 @@ function App() {
 
       <div className="app">
         <Header isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+        {isMenuOpen && <Menu />}
       </div>
     </Router>
   );
